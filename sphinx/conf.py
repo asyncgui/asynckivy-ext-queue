d27815a24cb4c11f95c844eac1818fa1036380d2
Nattōsai Mitō <flow4re2c@gmail.com>
2023-12-05 20:14:16 +0900

    switch the documentation theme to furo

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -33,7 +33,7 @@ gettext_location = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ['_static']
 # html_theme_options = {
 #     "use_repository_button": True,
